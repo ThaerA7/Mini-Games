56f48fdb73f0f22760c5cfc07375a8f73840fc25
ThaerA7 <thaeraran7@gmail.com>
2025-10-19 00:05:54 +0200

    Sudoku killer

--- web/src/pages/KillerSudokuPage.tsx
+++ web/src/pages/KillerSudokuPage.tsx
@@ -1,10 +1,29 @@
 // src/pages/KillerSudokuPage.tsx
-import KillerSudokuBoard from "../components/sudoku/KillerSudokuBoard.tsx";
+import TopBar from "../components/TopBar";
+import KillerSudokuBoard from "../components/sudoku/KillerSudokuBoard";
 
 export default function KillerSudokuPage() {
   return (
-    <div style={{ padding: 16 }}>
-      <KillerSudokuBoard />
+    <div
+      style={{
+        minHeight: "100dvh",
+        background: "#0b1020",
+        color: "#e5e7eb",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <TopBar />
+      <main
+        style={{
+          flex: 1,
+          display: "grid",
+          placeItems: "center",
+          padding: 16,
+        }}
+      >
+        <KillerSudokuBoard />
+      </main>
     </div>
   );
 }
